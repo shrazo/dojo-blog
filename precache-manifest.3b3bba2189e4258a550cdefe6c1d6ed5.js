e0af5308e3f4a51b2d190200188f0542e2c3938d
raju024-dev <raju024.dev@gmail.com>
2022-10-20 17:24:22 +0800

    Updates

---
+++ precache-manifest.3b3bba2189e4258a550cdefe6c1d6ed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16416542a02e1ba5dafa",
+    "url": "/dojo-blog/static/css/main.e8b34928.chunk.css"
+  },
+  {
+    "revision": "16416542a02e1ba5dafa",
+    "url": "/dojo-blog/static/js/main.16416542.chunk.js"
+  },
+  {
+    "revision": "55efea02c402689acb63",
+    "url": "/dojo-blog/static/js/1.55efea02.chunk.js"
+  },
+  {
+    "revision": "ca4c7eeccf69062a5545",
+    "url": "/dojo-blog/static/js/runtime~main.ca4c7eec.js"
+  },
+  {
+    "revision": "d766b0483e733fbfb9b8d7129b4be1df",
+    "url": "/dojo-blog/index.html"
+  }
+];
